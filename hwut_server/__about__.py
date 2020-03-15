6b6b558054bd7ce5cf25081e74d3b4c95b774b38
Raphael Lehmann <raphael@rleh.de>
2020-03-16 00:15:50 +0100

    Python package: fix __about__ git info

--- hwut_server/__about__.py
+++ hwut_server/__about__.py
@@ -11,11 +11,13 @@ __email__ = 'raphael+hwut@rleh.de'
 
 __license__ = 'AGPLv3'
 
+git_hash = 'no_git'
 try:
     git_hash = subprocess.check_output(['git', 'describe', '--always', '--dirty']).decode('utf-8').strip()
 except:
     pass
 
+git_branch = 'no_git'
 try:
     git_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('utf-8').strip()
 except:
